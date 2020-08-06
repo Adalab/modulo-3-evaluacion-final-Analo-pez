41a9a305c7a6289fc6ffcd0a9d66640de4ee62f9
Analo-pez <ana.ts.lopez@gmail.com>
2020-08-06 18:57:44 +0200

    create folder

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { fetchReasons } from '../services/ReasonsService';
+import Header from './Header'
+import '../stylesheets/App.scss';
+
+function App() {
+  return (
+    <div className="App ">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../stylesheets/Header.scss';
+
+function Header() {
+    return (
+        <div className="DivHeader">Preparando una buena estructura de REACT</div>
+    );
+}
+
+export default Header;
