f680ae42260ee682f8317661e558994fbc4eb484
Analo-pez <ana.ts.lopez@gmail.com>
2020-08-10 09:44:40 +0200

    add message error

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import CharacterCard from './CharacterCard';
+import PropTypes from "prop-types";
 
 const CharacterList = props => {
+    if (props.characters.length === 0) {
+        return <p className="Alert">No hay coincidencias</p>
+    }
     const htmlCode = props.characters.map((character, index) => {
         return <CharacterCard
             key={index}
@@ -9,9 +13,23 @@ const CharacterList = props => {
             image={character.image}
             name={character.name}
             species={character.species} />
-    });
+    })
+
     return (
         <section className="list">{htmlCode} </section>);
 };
+
+CharacterList.propTypes = {
+    id: PropTypes.number,
+    name: PropTypes.string,
+    image: PropTypes.string,
+    status: PropTypes.string,
+    specie: PropTypes.string,
+    origin: PropTypes.string,
+    gender: PropTypes.string,
+    episodes: PropTypes.number,
+};
+
+
 
 export default CharacterList;
